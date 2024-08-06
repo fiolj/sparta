06843755c217c9373cef8f745d3169c2cedf6f42
Juan Fiol <fiol@cab.cnea.gov.ar>
2024-08-06 16:17:28 -0300

    avancé. Tengo separados párrafos y comandos

--- workbench/parse_sjplimptxt.py
+++ workbench/parse_sjplimptxt.py
@@ -1,26 +1,70 @@
 from pathlib import Path
-parentdir = Path("../..")
+import re
+parentdir = Path().home() / "Trabajos/dsmc"  # Parent directory
 pi = parentdir / "sparta-orig/doc"  # Input path
-po = parentdir / "sparta/ newdoc"             # Output path
+po = parentdir / "sparta/newdoc"             # Output path
+
+repar = re.compile(r"(?s)((?:[^\n][\n]?)+)")  # Matches a paragraph
+recmd = re.compile(r".*:\S+$")
 
 if not po.exists():
   po.mkdir()
 
-format_par = ["p", "b", "pre", "c", "h1", "h2", "h3", "h4", "h5", "h6"]  # format the entire paragraph
-format_list = ["ul", "ol", "dl"]  # lines of the paragraph as a list
-format_parlist = ["l", "dt", "dd", "ulb", "ule", "olb", "ole", "dlb", "dle"]  # treat the paragraph as one entry in a list
-format_linepar = ["all(p)", "all(c)", "all(b)", "all(l)"]  # applied to each line of the paragraph
+cmd_par = ["p", "b", "pre", "c", "h1", "h2", "h3", "h4", "h5", "h6"]  # cmd the entire paragraph
+cmd_list = ["ul", "ol", "dl"]  # lines of the paragraph as a list
+cmd_parlist = ["l", "dt", "dd", "ulb", "ule", "olb", "ole", "dlb", "dle"]  # treat the paragraph as one entry in a list
+cmd_linepar = ["all(p)", "all(c)", "all(b)", "all(l)"]  # applied to each line of the paragraph
 
-cmd_special = [
-  "line",  # insert a horizontal line = <HR>
-  "image",  # insert an image = <IMG SRC = "file">
-  "link"  # insert a named link that can be referred to elsewhere
-]
+# cmd_special = [
+#   "line",
+#   "image",  # insert an image = <IMG SRC = "file">
+#   "link"  # insert a named link that can be referred to elsewhere
+# ]
 
-# s = pi.read_text()
-fi = pi / "Section_accelerate.txt"
-for line in fi.read_text().splitlines():
-  L = line.split(":")
-  # print(line)
-  if len(L) > 1:
-    print(L)
+cmd_args = ["link", "tb", "image"]
+cmds = cmd_par + cmd_list + cmd_parlist + cmd_linepar + ['line']
+
+scmds = "|".join(cmds).replace("(",r"\(").replace(")",r"\)")
+recmds = re.compile(":(("+scmds+"|,"+")+)$")
+recmdargs = re.compile(':('+'|'.join(cmd_args)+r')\([^\)]+\)')
+
+
+# fi = pi / "Section_accelerate.txt"
+fi = pi / "fix.txt"
+
+
+def get_par_cmds(paragraph):
+  """Return the paragraph and commands
+
+
+
+  Parameters
+  ----------
+  paragraph : text to analyze
+   
+
+  Returns
+  -------
+   
+  tuple: (paragraph,[cmds])
+  """
+
+  m = recmds.search(paragraph)
+  if not m:
+    m = recmdargs.search(paragraph)
+  if m:
+    parrafo = paragraph[:m.start()]
+    cmd = paragraph[m.start()+1:m.end()].split(",")
+    return parrafo, cmd
+  else:
+    return paragraph,[]
+
+if __name__ == '__main__':
+
+  s = fi.read_text().replace(':link(','\n:link(')
+
+  for j, m in enumerate(repar.finditer(s)):
+    par, cmds = get_par_cmds(m.group().rstrip())
+    if len(cmds) >= 1:
+      print(cmds)
+
