38d23fc846dca0dccae18382702e9243976d9aaf
Juan Fiol <fiol@cab.cnea.gov.ar>
2024-08-07 10:17:20 -0300

    Starting appl ying formatting commands

--- workbench/parse_sjplimptxt.py
+++ workbench/parse_sjplimptxt.py
@@ -10,29 +10,21 @@ if not po.exists():
   po.mkdir()
 
 cmd_par = ["p", "b", "pre", "c", "h1", "h2", "h3", "h4", "h5", "h6"]  # cmd the entire paragraph
-cmd_list = ["ul", "ol", "dl"]  # lines of the paragraph as a list
-cmd_parlist = ["l", "dt", "dd", "ulb", "ule", "olb", "ole", "dlb", "dle"]  # treat the paragraph as one entry in a list
+cmd_lst = ["ul", "ol", "dl"]  # lines of the paragraph as a lst
+cmd_parlst = ["l", "dt", "dd", "ulb", "ule", "olb", "ole", "dlb", "dle"]  # treat the paragraph as one entry in a lst
 cmd_linepar = ["all(p)", "all(c)", "all(b)", "all(l)"]  # applied to each line of the paragraph
-cmd_args = ["link", "tb", "image"]                      # Commands that take arguments
 cmd_indep = ['line']            # Commands independent of paragraphs
-
-cmds = cmd_par + cmd_list + cmd_parlist + cmd_linepar + cmd_indep
+cmd_args = ["link", "tb", "image"]                      # Commands that take arguments
+cmds = cmd_par + cmd_lst + cmd_parlst + cmd_linepar + cmd_indep  # All commands with no arguments
 
 scmds = "|".join(cmds).replace("(",r"\(").replace(")",r"\)")
-scmdargs = '|('+'|'.join(cmd_args)+r')\([^\)]+\)'
-# recmds = re.compile(":(("+scmds+"|," + ")+)$")
-# recmdargs = re.compile(':('+'|'.join(cmd_args)+r')\([^\)]+\)')
-srecmds = ":("+ scmds  + scmdargs  + "|," + ")+"
+scmdargs = '('+'|'.join(cmd_args)+r')\([^\)]+\)'
+
+# Regular expressions
+srecmds = f":({scmds}|{scmdargs}|,)+"
 recmds = re.compile(f"({srecmds}$)",flags=re.M)
-
-
-# repar = re.compile(rf"(?s)((?:[^\n][\n]?)+|{srecmds})")  # Matches a paragraph
-# repar = re.compile(r"(?s)((?:[^\n][\n]?)+)")  # Matches a paragraph
 repar = re.compile(r"(?s)((?:[^\n][\n]?)+)")  # Matches a paragraph
 
-# fi = pi / "Section_accelerate.txt"
-fi = pi / "Section_howto.txt"
-fi = pi / "fix.txt"
 
 def read_sjptxt(fi):
   """Read txt file and preprocess the contents
@@ -51,6 +43,7 @@ def read_sjptxt(fi):
   """
   s = Path(fi).read_text()
   s = recmds.sub(r"\1\n",s)     # Add newline after each command
+  s = s.replace("\\\n","")      # Remove continuation lines
   return s
 
 def get_par_cmds(paragraph):
@@ -75,12 +68,28 @@ def get_par_cmds(paragraph):
   else:
     return paragraph,[]
 
+apply_cmds = {
+  'p':lambda x:x,
+  'b':lambda x:x,
+  'line': lambda x: "------",
+  
+}  
 if __name__ == '__main__':
 
+  fi = pi / "Section_accelerate.txt"
+  fi = pi / "Section_howto.txt"
+  # fi = pi / "fix.txt"
+  # fi = pi / "variable.txt"
+  # for fi in pi.glob("*.txt"):
+  #   s = read_sjptxt(fi)
   s = read_sjptxt(fi)
+  ss = ""
 
   for m in repar.finditer(s):
     par, cmds = get_par_cmds(m.group().rstrip())
+    # ss = ""
+    
+    # ss += apply_cmd_par(s)
     if len(cmds) >= 1:
       print(cmds)
 
