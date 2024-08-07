f8826f1bbe42f45ad94c2961d822760a94b41093
Juan Fiol <fiol@cab.cnea.gov.ar>
2024-08-07 16:16:04 -0300

    It works (apparently), missing commands with args

--- workbench/parse_sjplimptxt.py
+++ workbench/parse_sjplimptxt.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import re
 parentdir = Path().home() / "Trabajos/dsmc"  # Parent directory
 pi = parentdir / "sparta/doc"  # Input path
-po = parentdir / "sparta/newdoc"             # Output path
+po = parentdir / "sparta/docs"             # Output path
 
 # recmd = re.compile(r".*:\S+$")
 
@@ -22,8 +22,38 @@ scmdargs = '('+'|'.join(cmd_args)+r')\([^\)]+\)'
 
 # Regular expressions
 srecmds = f":({scmds}|{scmdargs}|,)+"
+# recmdargs = re.compile(f"({scmdargs}$)",flags=re.M)
 recmds = re.compile(f"({srecmds}$)",flags=re.M)
 repar = re.compile(r"(?s)((?:[^\n][\n]?)+)")  # Matches a paragraph
+
+rebraceS = re.compile('([[])([^]]+:)[]]',re.M)
+rebrace = re.compile('([{[])([^]}]+)[]}]',re.M)
+
+def apply_inmrkp(s):
+  """Convert markup to rst
+
+  Convert [blah:] -> heading-6
+  Convert [blah] -> **blah**
+  Convert {blah} -> *blah*
+
+  Parameters
+  ----------
+  s : string
+  
+
+  Returns
+  -------
+  
+  string
+  """
+  def repl(m):
+    dmrkup = {'[':"**", '{':'*'}
+    delim = dmrkup[m.group(1)]
+    return f"{delim}{m.group(2).strip()}{delim}"
+
+  s1= rebraceS.sub(r"\2"+":h6\n\n",s)
+  s1= rebrace.sub(repl,s1)
+  return s1
 
 
 def read_sjptxt(fi):
@@ -68,28 +98,81 @@ def get_par_cmds(paragraph):
   else:
     return paragraph,[]
 
-apply_cmds = {
-  'p':lambda x:x,
-  'b':lambda x:x,
-  'line': lambda x: "------",
+def fmt_literal(s):
+  """format paragraph as code
+
+  Parameters
+  ----------
+  s : paragraph
   
-}  
+
+  Returns
+  -------
+  
+  string: formatted paragraph as code
+  """
+  ss = "::\n\n   "
+  L = s.splitlines()
+  ss += "\n   ".join(L)
+  return f"\n{ss}\n"
+
+
+apply_cmds = {
+  'p':lambda x:f"{x}",
+  'b':lambda x:f"{x}",
+  'c':lambda x:f"{x}",
+  # We use "standard" order for sections
+  'h1':lambda x:f"\n{len(x)*'#'}\n{x}\n{len(x)*'#'}\n",
+  'h2':lambda x:f"\n{len(x)*'*'}\n{x}\n{len(x)*'*'}\n",
+  'h3':lambda x:f"\n{x}\n{len(x)*'='}\n",
+  'h4':lambda x:f"\n{x}\n{len(x)*'-'}\n",
+  'h5':lambda x:f"\n{x}\n{len(x)*'^'}\n",
+  'h6':lambda x:f"\n{x}\n{len(x)*'\"'}\n",
+  'pre':fmt_literal,
+  'ulb':lambda x: f"{x}\n",
+  'ule':lambda x: f"{x}\n\n",
+  'olb':lambda x: f"{x}\n",
+  'ole':lambda x: f"{x}\n\n",
+  'l': lambda x: f"- {x}",
+  'line': lambda x: "\n--------------\n",
+  'all(p)': lambda x: '\n\n'.join(x.splitlines())+'\n',
+  'all(b)':lambda x:f"{x}\n",
+  'all(l)': lambda x: '\n-'.join(x.splitlines())+'\n',
+  # 'link':lambda x:x,
+}
+
+
 if __name__ == '__main__':
 
-  fi = pi / "Section_accelerate.txt"
-  fi = pi / "Section_howto.txt"
-  # fi = pi / "fix.txt"
-  # fi = pi / "variable.txt"
+  fn = "Section_accelerate.txt"
+  fn = "Section_howto.txt"
+  fn = "fix.txt"
+  fn = "variable.txt"
+  fn = "write_restart.txt"
   # for fi in pi.glob("*.txt"):
   #   s = read_sjptxt(fi)
+  fi = pi/fn
   s = read_sjptxt(fi)
+  s = apply_inmrkp(s)
+
   ss = ""
 
   for m in repar.finditer(s):
-    par, cmds = get_par_cmds(m.group().rstrip())
-    # ss = ""
+    par, commds = get_par_cmds(m.group().rstrip())
+    # if len(commds) >= 1:
+    #   print(f"++{par}++")
+    #   print(commds)
+    ppar = par.rstrip()
+    for c in commds:
+      if c in cmds:
+        ppar = apply_cmds[c](ppar)
     
-    # ss += apply_cmd_par(s)
-    if len(cmds) >= 1:
-      print(cmds)
+    # for c in commds:
+    #   if c in cmds:
+    #     ppar = apply_cmds[c](ppar)
+    #   else:
+    #     ppar = par
+    ss += f"{ppar}\n"
+    # # ss += apply_cmd_par(s)
+    (po / fn).with_suffix('.rst').write_text(ss)
 
