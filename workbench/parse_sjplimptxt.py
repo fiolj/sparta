4a4f029a9f26e1053e31faa758c0fb9014c68671
Juan Fiol <juanfiol@gmail.com>
2024-08-07 23:53:48 -0300

    Fixed some links

--- workbench/parse_sjplimptxt.py
+++ workbench/parse_sjplimptxt.py
@@ -17,17 +17,18 @@ cmd_indep = ['line']            # Commands independent of paragraphs
 cmd_args = ["link", "tb", "image"]                      # Commands that take arguments
 cmds = cmd_par + cmd_lst + cmd_parlst + cmd_linepar + cmd_indep  # All commands with no arguments
 
-scmds = "|".join(cmds).replace("(",r"\(").replace(")",r"\)")
-scmdargs = '('+'|'.join(cmd_args)+r')\([^\)]+\)'
+scmds = "|".join(cmds).replace("(", r"\(").replace(")", r"\)")
+scmdargs = '('+'|'.join(cmd_args)+r')(\([^\)]+\))'
 
 # Regular expressions
 srecmds = f":({scmds}|{scmdargs}|,)+"
 # recmdargs = re.compile(f"({scmdargs}$)",flags=re.M)
-recmds = re.compile(f"({srecmds}$)",flags=re.M)
+recmds = re.compile(f"({srecmds}$)", flags=re.M)
 repar = re.compile(r"(?s)((?:[^\n][\n]?)+)")  # Matches a paragraph
 
-rebraceS = re.compile('([[])([^]]+:)[]]',re.M)
-rebrace = re.compile('([{[])([^]}]+)[]}]',re.M)
+rebraceS = re.compile('([[])([^]]+:)[]]', re.M)
+rebrace = re.compile('([{[])([^]}]+)[]}]', re.M)
+
 
 def apply_inmrkp(s):
   """Convert markup to rst
@@ -39,20 +40,20 @@ def apply_inmrkp(s):
   Parameters
   ----------
   s : string
-  
+
 
   Returns
   -------
-  
+
   string
   """
   def repl(m):
-    dmrkup = {'[':"**", '{':'*'}
+    dmrkup = {'[': "**", '{': '*'}
     delim = dmrkup[m.group(1)]
-    return f"{delim}{m.group(2).strip().replace("*",r"\*")}{delim}"
+    return f"{delim}{m.group(2).strip().replace("*", r"\*")}{delim}"
 
-  s1= rebraceS.sub(r"\2"+":h6\n\n",s)
-  s1= rebrace.sub(repl,s1)
+  s1 = rebraceS.sub(r"\2"+":h6\n\n", s)
+  s1 = rebrace.sub(repl, s1)
   return s1
 
 
@@ -62,17 +63,20 @@ def read_sjptxt(fi):
   Parameters
   ----------
   fi : pathlib Path, file object or string
-  
+
 
   Returns
   -------
-  
+
   string: Contents of file
   """
   s = Path(fi).read_text()
-  s = recmds.sub(r"\1\n",s)     # Add newline after each command
-  s = s.replace("\\\n","")      # Remove continuation lines
+  s = recmds.sub(r"\1\n", s)     # Add newline after each command
+  s = s.replace("\\\n", "")      # Remove continuation lines
+  s = cleanup_header(s)
+  s = apply_inmrkp(s)
   return s
+
 
 def get_par_cmds(paragraph):
   """Return the paragraph and commands
@@ -80,33 +84,35 @@ def get_par_cmds(paragraph):
   Parameters
   ----------
   paragraph : text to analyze
-   
+
 
   Returns
   -------
-   
+
   tuple: (paragraph,[cmds])
   """
 
   m = recmds.search(paragraph)
   if m:
     parrafo = paragraph[:m.start()]
-    cmd = paragraph[m.start()+1:m.end()].split(",")
-    return parrafo, cmd
+    tt = paragraph[m.start()+1:m.end()]
+    cmd = re.sub(r'\([^\)]+\)', lambda x: x.group(0).replace(',', ';'), tt).split(',')
+    return parrafo, [c.replace(';', ',') for c in cmd]
   else:
-    return paragraph,[]
+    return paragraph, []
 
-def fmt_literal(s):
+
+def fmt_literal(c, s):
   """format paragraph as code
 
   Parameters
   ----------
   s : paragraph
-  
+
 
   Returns
   -------
-  
+
   string: formatted paragraph as code
   """
   ss = "::\n\n   "
@@ -115,30 +121,52 @@ def fmt_literal(s):
   return f"\n{ss}\n"
 
 
+def fmt_link(c, p):
+  """Format link in rst format
+
+  Parameters
+  ----------
+  s : string
+
+
+  Returns
+  -------
+
+  string
+  """
+  # print(c)
+  args = (c[5:-1]).split()
+  if len(args) == 1:            #
+    t = f".. _{args[0]}:\n\n{p}"
+  return t
+
+
 apply_cmds = {
-  'p':lambda x:f"{x}",
-  'b':lambda x:f"{x}",
-  'c':lambda x:f"{x}",
+  'p': lambda c, x: f"{x}",
+  'b': lambda c, x: f"{x}",
+  'c': lambda c, x: f"{x}",
   # We use "standard" order for sections
-  'h1':lambda x:f"\n{len(x)*'#'}\n{x}\n{len(x)*'#'}\n",
-  'h2':lambda x:f"\n{len(x)*'*'}\n{x}\n{len(x)*'*'}\n",
-  'h3':lambda x:f"\n{x}\n{len(x)*'='}\n",
-  'h4':lambda x:f"\n{x}\n{len(x)*'-'}\n",
-  'h5':lambda x:f"\n{x}\n{len(x)*'^'}\n",
-  'h6':lambda x:f"\n{x}\n{len(x)*'\"'}\n",
-  'pre':fmt_literal,
-  'ul':lambda x: f"{x}\n",
-  'ulb':lambda x: f"{x}\n",
-  'ule':lambda x: f"{x}\n\n",
-  'olb':lambda x: f"{x}\n",
-  'ole':lambda x: f"{x}\n\n",
-  'l': lambda x: f"- {x.replace('\n',' ')}",
-  'line': lambda x: "\n--------------\n",
-  'all(p)': lambda x: '\n\n'.join(x.splitlines())+'\n',
-  'all(b)':lambda x:f"{x}\n",
-  'all(l)': lambda x: '\n-'.join(x.splitlines())+'\n',
-  # 'link':lambda x:x,
+  'h1': lambda c, x: f"\n{len(x)*'#'}\n{x}\n{len(x)*'#'}\n",
+  'h2': lambda c, x: f"\n{len(x)*'*'}\n{x}\n{len(x)*'*'}\n",
+  'h3': lambda c, x: f"\n{x}\n{len(x)*'='}\n",
+  'h4': lambda c, x: f"\n{x}\n{len(x)*'-'}\n",
+  'h5': lambda c, x: f"\n{x}\n{len(x)*'^'}\n",
+  'h6': lambda c, x: f"\n{x}\n{len(x)*'\"'}\n",
+  'pre': fmt_literal,
+  'ul': lambda c, x: f"{x}\n",
+  'ulb': lambda c, x: f"{x}\n",
+  'ule': lambda c, x: f"{x}\n\n",
+  'olb': lambda c, x: f"{x}\n",
+  'ole': lambda c, x: f"{x}\n\n",
+  'l': lambda c, x: f"- {x.replace('\n', ' ')}",
+  # 'line': lambda c, x: "\n--------------\n",
+  'line': lambda c, x: "\n",
+  'all(p)': lambda c, x: '\n\n'.join(x.splitlines())+'\n',
+  'all(b)': lambda c, x: f"{x}\n",
+  'all(l)': lambda c, x: '\n-'.join(x.splitlines())+'\n',
+  'link': fmt_link,
 }
+
 
 def cleanup_header(s):
   """Remove standard SPARTA header
@@ -147,18 +175,20 @@ def cleanup_header(s):
   Parameters
   ----------
   s : string. Contents of file
-  
+
 
   Returns
   -------
-  
+
   string:
   """
-  headers = ['."Previous Section"_Section_commands.html - "SPARTA WWW Site"_sws - "SPARTA Documentation"_sd - "SPARTA Commands"_sc - "Next Section"_Section_example.html :c\n\n\n:link(sws,http://sparta.sandia.gov)\n\n:link(sd,Manual.html)\n\n:link(sc,Section_commands.html#comm)\n\n\n:line \n\n',
-             '"SPARTA WWW Site"_sws - "SPARTA Documentation"_sd - "SPARTA Commands"_sc :c\n\n\n:link(sws,http://sparta.sandia.gov)\n\n:link(sd,Manual.html)\n\n:link(sc,Section_commands.html#comm)\n\n\n:line\n\n'             
+  headers = ['."Previous Section"_Section_commands.html - "SPARTA WWW Site"_sws - "SPARTA Documentation"_sd - "SPARTA Commands"_sc - "Next Section"_Section_example.html :c\n\n\n:link(sws,http://sparta.sandia.gov)\n\n:link(sd,Manual.html)\n\n:link(sc,Section_commands.html#comm)\n\n\n:line',
+             '"Previous Section"_Manual.html - "SPARTA WWW Site"_sws - "SPARTA Documentation"_sd - "SPARTA Commands"_sc - "Next Section"_Section_start.html :c\n\n\n:link(sws,http://sparta.sandia.gov)\n\n:link(sd,Manual.html)\n\n:link(sc,Section_commands.html#comm)\n\n\n:line',
+             '"SPARTA WWW Site"_sws - "SPARTA Documentation"_sd - "SPARTA Commands"_sc :c\n\n\n:link(sws,http://sparta.sandia.gov)\n\n:link(sd,Manual.html)\n\n:link(sc,Section_commands.html#comm)\n\n\n:line'
              ]
-  for h in headers:
-    s =s.replace(h,'')
+  # for h in headers:
+  #   s = s.replace(h, '')
+  s = re.sub(r'[\.]?"Previous .+?:line', '', s, flags=re.DOTALL)
   return s
 
 # def skip_paragraph(s):
@@ -168,46 +198,78 @@ def cleanup_header(s):
 
 #   Parameters
 #   ----------
-#   s : 
-  
+#   s :
+
 
 #   Returns
 #   -------
-  
+
 #   bool:
 #   """
-#   orig_header1 = """"SPARTA WWW Site"_sws - "SPARTA Documentation"_sd - "SPARTA Commands"_sc :c:link(sws,http://sparta.sandia.gov):link(sd,Manual.html):link(sc,Section_commands.html#comm):line"""  
+# orig_header1 = """"SPARTA WWW Site"_sws - "SPARTA Documentation"_sd -
+# "SPARTA Commands"_sc
+# :c:link(sws,http://sparta.sandia.gov):link(sd,Manual.html):link(sc,Section_commands.html#comm):line"""
 
 #   return par.replace(' ','') != orig_header1
-#   return not any(t in s for t in ['"SPARTA WWW Site"','sws,http://sparta.sandia.gov','sd,Manual.html','sc,Section_commands.html']) 
+#   return not any(t in s for t in ['"SPARTA WWW Site"','sws,http://sparta.sandia.gov','sd,Manual.html','sc,Section_commands.html'])
 
-def create_link(s):
+def make_linkname(s):
   """Create a target link from a title
 
   Remove short words and spaces
 
   Parameters
   ----------
-  s : 
-  
+  s :
+
 
   Returns
   -------
-  
+
   string: a target name
   """
   ignore = ['a', 'and', 'de', 'des', 'for', 'in', 'of', 'on', 'the']
   return (''.join([x for x in s.split() if x.lower() not in ignore]))
 
 
+def apply_cmd_fmt(s):
+  """Apply formatting commands to text
+
+  Parameters
+  ----------
+  s : string. Pre-processed contents of file
+
+
+  Returns
+  -------
+
+  string: Text with applied formats
+  """
+
+  ss = ""
+  for m in repar.finditer(s):
+    par, commds = get_par_cmds(m.group().rstrip())
+    # if len(commds) >= 1:
+    #   print(f"++{par}++")
+    #   print(commds)
+    ppar = par.rstrip()
+    for c in commds:
+
+      if c in cmds:
+        ppar = apply_cmds[c](c, ppar)
+      elif 'link' in c:
+        ppar = apply_cmds['link'](c, ppar)
+    ss += f"{ppar}\n\n"
+  return ss
+
 
 if __name__ == '__main__':
 
   fn = "Section_accelerate.txt"
-  fn = "Section_howto.txt"
   fn = "fix.txt"
   fn = "variable.txt"
   fn = "write_restart.txt"
+  fn = "Section_intro.txt"
   fn = "Section_howto.txt"
   fi = pi/fn
 
@@ -215,21 +277,7 @@ if __name__ == '__main__':
   #   s = read_sjptxt(fi)
 
   s = read_sjptxt(fi)
-  s = cleanup_header(s)
-  s = apply_inmrkp(s)
 
-  ss = ""
+  s1 = apply_cmd_fmt(s)
 
-  for m in repar.finditer(s):
-    par, commds = get_par_cmds(m.group().rstrip())
-    # if len(commds) >= 1:
-    #   print(f"++{par}++")
-    #   print(commds)
-    if skip_paragraph(par):
-      ppar = par.rstrip()
-      for c in commds:
-        if c in cmds:
-          ppar = apply_cmds[c](ppar)
-      ss += f"{ppar}\n\n"
-    
-  (po / fi.name).with_suffix('.rst').write_text(ss)
+  (po / f"{fi.name}_0").with_suffix('.rst').write_text(s1)
